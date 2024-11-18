4fc3e59e0ae938ceb27e236c3d9f4bf4a6abc4c5
DESKTOP-VEBS053\ssm <kms04421@naver.com>
2024-11-19 00:31:30 +0900

    송수민 ADD :  모바일로 project Setting , build Setting  적용

--- My project/Assets/Script/Player/Weapon/WeaponController.cs
+++ My project/Assets/Script/Player/Weapon/WeaponController.cs
@@ -63,7 +63,7 @@ namespace MainSSM
                 
             }
             isAttacking = false;
-            ResetWeaponPosition();
+            ResetWeaponPosition(); 
         }
         public bool ResetWeaponPosition()
         {
